74362e90960eef1989a88e286e351298a49f7a06
Rodrigo Cantu <rcantuv@berkeley.edu>
2025-08-29 16:41:38 -0700

    feat: fetch data

---
+++ data/assignments.js
@@ -0,0 +1,59 @@
+assignments = {
+  "CS161": [
+    {
+      "title": "Homework 1",
+      "course": "CS161",
+      "dueDate": "2025-09-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/1098878/assignments/6580541/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Project 1 Autograder",
+      "course": "CS161",
+      "dueDate": "2025-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/1098878",
+      "submitted": false
+    },
+    {
+      "title": "Project 1 Checkpoint",
+      "course": "CS161",
+      "dueDate": "2025-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/1098878",
+      "submitted": false
+    },
+    {
+      "title": "Project 1 Writeup",
+      "course": "CS161",
+      "dueDate": "2025-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/1098878",
+      "submitted": false
+    }
+  ],
+  "EE16A-F25": [
+    {
+      "title": "HW0",
+      "course": "EE16A-F25",
+      "dueDate": "2025-08-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/1095473",
+      "submitted": false
+    }
+  ],
+  "MATH 113-LEC-001": [
+    {
+      "title": "HW1",
+      "course": "MATH 113-LEC-001",
+      "dueDate": "2025-09-02T14:10:00",
+      "link": "https://www.gradescope.com/courses/1072860",
+      "submitted": false
+    }
+  ],
+  "Phys110a": [
+    {
+      "title": "Homework 1",
+      "course": "Phys110a",
+      "dueDate": "2025-09-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/1106387",
+      "submitted": false
+    }
+  ]
+};
