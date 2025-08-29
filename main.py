16e8eaa6c5e1d9cf0d3b2b514746aec39f2aa773
Rodrigo Cantu <rcantuv@berkeley.edu>
2025-08-29 16:28:05 -0700

    fix: prevent incorrect .yml file loading

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ for module in modules:
     module.run(assignments)
 
 # Update GitHub workflow with all environment variables
-workflow_template = utils.load_template('workflow_template.yml')
-envs = [x + ': ${{ secrets.' + x + ' }}' for x in Module.envs]
-workflow = workflow_template.replace(
-    '__ENV__',
-    ('\n' + ' ' * 8).join(envs)
-)
-with open(os.path.join('.github', 'workflows', 'main.yml'), 'w') as file:
-    file.write(workflow)
+# workflow_template = utils.load_template('workflow_template.yml')
+# envs = [x + ': ${{ secrets.' + x + ' }}' for x in Module.envs]
+# workflow = workflow_template.replace(
+#     '__ENV__',
+#     ('\n' + ' ' * 8).join(envs)
+# )
+# with open(os.path.join('.github', 'workflows', 'main.yml'), 'w') as file:
+#     file.write(workflow)
 
 # Update README.md with valid secret names
 readme_template = utils.load_template('readme_template.md')
